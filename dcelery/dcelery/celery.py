205d0e382b4159b216c1729c51e43b98c6d09f46
akash <akash9122000@gmail.com>
2025-06-10 17:14:07 +0530

    start creating task for the celery.

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -5,8 +5,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 
 app = Celery("dcelery")
 
-app.conf.from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 @app.task
 def add_numbers():
     return
+
+app.autodiscover_tasks()
